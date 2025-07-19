ec0137ce7d09684fa9c7ed9465970b6c7e6b1bf0
1borkot <bkuxyz@gmail.com>
2025-07-20 00:00:32 +0600

    Creating secnd branc for final product

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from app.routes import donate, history
+
 
 app = FastAPI()
 
-app.include_router(donate.router)
-app.include_router(history.router)
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Khayru Ummah Foundation"}
+
--- app/routes/history.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Query
-from app import crud
-
-router = APIRouter()
-
-@router.get("/history")
-async def get_history(email: str = Query(..., description="Email to lookup donations")):
-    donations = await crud.get_donations_by_email(email)
-    return {"email": email, "donations": donations}
