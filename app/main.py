7dc123c9f303cbac4a6e4e45a787cfd44e963752
1borkot <bkuxyz@gmail.com>
2025-07-20 18:55:45 +0600

    learning to build

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from hmac import new
+from operator import ne
 from fastapi import FastAPI, Body, Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
@@ -8,10 +10,11 @@ import time
 
 
 app = FastAPI()
+
 class Post(BaseModel):
     title: str
     content: str
-    Published: bool = True
+    published: bool = True
 
 
 while True:
@@ -30,15 +33,18 @@ while True:
         print(f"Error: {error}")
         time.sleep(2)
 
+
 my_posts = [
     {"title": "First Post", "content": "This is the content of the first post", "id": 1}, 
     {"title": "Second Post", "content": "This is the content of the second post", "id": 2}
 ]
 
+
 def find_post(id: int):
     for p in my_posts:
         if p['id'] == id:
             return p
+
 
 def find_post_index(id: int):
     for i, p in enumerate(my_posts):
@@ -46,25 +52,31 @@ def find_post_index(id: int):
             return i
         
 
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to Khayru Ummah Foundation"}
 
+
 @app.get("/posts")
 async def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts=cursor.fetchall()
+    return {"data": posts}
+
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def create_posts(post: Post,):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",(post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post} 
 
 
 @app.get("/posts/{id}")
 async def get_post(id: int, response: Response):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (id,))
+    post = cursor.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found")
     return { "post_details": post}
@@ -72,20 +84,21 @@ async def get_post(id: int, response: Response):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id : int): 
-    index = find_post_index(id)
-    if index is None:
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if deleted_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found")
 
-    my_posts.pop(index)#type: ignore
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
 async def update_post(id: int, post: Post):
-    index = find_post_index(id)
-    if index is None:
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, id))
+
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found")
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"data": post_dict}
+    return {"data": updated_post} 
